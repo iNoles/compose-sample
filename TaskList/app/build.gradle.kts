fe2a25594d9d557f129af5084445953654a2131b
iNoles <iNoles@users.noreply.github.com>
2021-08-06 00:10:02 -0400

    Updated Compose Plugin to 1.1.0-alpha01

--- TaskList/app/build.gradle.kts
+++ TaskList/app/build.gradle.kts
@@ -75,20 +75,22 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.6.0")
 
-    implementation("androidx.activity:activity-compose:1.3.0")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha05")
+    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha06")
 
     implementation("androidx.compose.ui:ui:${rootProject.extra["composeVersion"]}")
     implementation("androidx.compose.material:material:${rootProject.extra["composeVersion"]}")
-    implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["composeVersion"]}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["composeVersion"]}")
 
     implementation("androidx.room:room-ktx:${rootProject.extra["roomVersion"]}")
     kapt("androidx.room:room-compiler:${rootProject.extra["roomVersion"]}")
 
-    implementation("com.google.accompanist:accompanist-pager-indicators:0.15.0")
+    implementation("com.google.accompanist:accompanist-pager-indicators:0.16.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["composeVersion"]}")
+    debugImplementation("androidx.compose.ui:ui-tooling:${rootProject.extra["composeVersion"]}")
+
 }
--- TaskList/build.gradle.kts
+++ TaskList/build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val composeVersion by extra("1.0.0")
+    val composeVersion by extra("1.1.0-alpha01")
     val roomVersion by extra("2.4.0-alpha04")
     repositories {
         google()
@@ -8,7 +8,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
